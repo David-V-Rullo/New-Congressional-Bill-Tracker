4fe5de33548701cf165c8a14045a933fcb373fa4
Patrick Braun <prbraun@protonmail.com>
2021-06-16 13:31:07 -0500

    A date search route is working given a url and api key

--- controllers/api/ext-routes.js
+++ controllers/api/ext-routes.js
@@ -1,6 +1,32 @@
 const router = require("express").Router();
-const {User} = require("../../models");
 const passport = require("../../config/passport");
+const axios = require('axios');
+
+
+// {
+// 	"url": "https://api.propublica.org/congress/v1/bills/search.json?sort=date&dir=desc",
+//   "key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH"
+// }
+
+router.get("/date-search", async (req, res) => {
+    console.log("GET /api/ext/date-search");
+    try {
+        var key = req.body.key;
+        console.log("THE URL: " + req.body.url);
+        const apiData = await axios.get(req.body.url, {headers: { "X-API-Key": key }});
+        // const apiData = await axios.get(
+        //     "https://api.propublica.org/congress/v1/bills/search.json?sort=date&dir=desc",
+        //     {
+        //         headers: { "X-API-Key": "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH" },
+        //     });
+        console.log(apiData.data);
+        res.status(200).json(apiData.data);
+        //res.status(200).json(apiData);
+    } catch (err) {
+        console.error(err);
+        res.status(401).json(err);
+    }
+});
 
 
 
