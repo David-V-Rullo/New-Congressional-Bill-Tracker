401369a3aea1edbbab52b4365029f597e604b321
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-17 10:42:35 -0400

    Working through some git issues

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const path = require('path');
 const express = require('express');
 const ejs = require('ejs')
-// Import express-session
-const session = require('express-session');
-// const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up sessions
-const sess = {
-  secret: 'Super secret secret',
-  resave: false,
-  saveUninitialized: true,
-};
+// const sess = {
+//   secret: 'Super secret secret',
+//   resave: false,
+//   saveUninitialized: true,
+// };
 
-// app.engine('handlebars', hbs.engine);
-app.set('views', './views')
 app.set('view engine', 'ejs');
 
 app.use(express.json());
@@ -27,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+ 
+// sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
+// });
