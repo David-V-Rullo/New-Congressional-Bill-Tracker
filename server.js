25d1fa89b21e2686e5104afdb116ad0a6c4fa4ec
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-18 08:31:55 -0400

    Having some extension issues in VS Code

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
+/* eslint-disable prettier/prettier */
 // Requiring necessary npm packages
 const express = require("express");
 const session = require("express-session");
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-
+const routes = require("./controllers");
+// const sequelize = require("./config/connection");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use("/css", express.static(__dirname + "public/css"));
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -25,15 +23,9 @@ app.use(passport.session());
 
 // Requiring our routes
 app.use(routes);
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Syncing our database and logging a message to the user upon success
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
+// sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => {
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);});
