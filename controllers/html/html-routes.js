c4c7ade46c7edcbd0b7f200bfa31dbf74cac1f45
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-19 11:15:33 -0400

    Signup Route is finished, adding buttons and functionality

--- controllers/html/html-routes.js
+++ controllers/html/html-routes.js
@@ -38,10 +38,14 @@ router.get("/login", (req, res) => {
   console.log("GET /login");
   // If the user already has an account send them to the members page
   if (req.user) {
-    res.redirect("/members");
+    res.redirect("/user");
   }
   res.render('login')
 });
+
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
 
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
