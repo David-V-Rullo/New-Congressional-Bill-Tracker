5114acdcd0099baf025a3cf3861866936a20d5a4
Patrick Braun <prbraun@protonmail.com>
2021-06-18 22:12:36 -0500

    can now logout from the user page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,4 +63,5 @@ router.get("/logout", (req, res) => {
   }
 });
 
+
 module.exports = router;
--- public/js/function.js
+++ public/js/function.js
@@ -2,20 +2,34 @@ var loginButton = $("#login-button")
 var homeButton = $("#home-button")
 var searchSubmit = $("#search-submit")
 var signupButton = $("#signup-button")
+var logoutButton = $("#logout-button")
 
 
-loginButton.click(function(e){
+loginButton.click(function (e) {
     window.location.replace('/login')
 })
 
-signupButton.click(function(e){
+signupButton.click(function (e) {
     window.location.replace('/signup')
 })
 
-homeButton.click(function(e) {
+homeButton.click(function (e) {
     console.log(e)
-    window.location.replace('/') })
+    window.location.replace('/')
+})
 
-searchSubmit.click(function(e) {
+searchSubmit.click(function (e) {
     window.location.replace('/default')
+})
+
+logoutButton.click(function (e) {
+    fetch("/api/user/logout")
+        .then(data => {
+            console.log("Logged Out");
+            window.location.replace("/");
+            
+        })
+        .catch(err => {
+            console.log("Failed to log out");
+        });
 })
