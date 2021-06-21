39dffb244d0308b37088dfd1b112d8c97bc257b8
Patrick Braun <prbraun@protonmail.com>
2021-06-21 16:02:36 -0500

    Login is now working properly

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
   // Getting references to our form and inputs
-  const loginForm = $("#login-submit");
+  //const loginForm = $("#login-submit");
+  const loginForm = $("#login-form-submit");
   const emailInput = $("#email-login");
   const passwordInput = $("#password-login");
 
