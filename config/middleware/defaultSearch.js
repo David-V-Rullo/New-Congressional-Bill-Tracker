7fec4ee745b9cbc53fa6241fffaa3be3f5821893
David-V-Rullo <d.v.rullo@gmail.com>
2021-06-21 08:34:16 -0400

    All of my working stuff to be merged to main branch.

--- config/middleware/defaultSearch.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function() {
-   var key = "jHHlm068RlyEusHIX91YA9zmZrvEtDyGplugF6tH";
-   const axios = require('axios') 
-   const results = await axios.get("https://api.propublica.org/congress/v1/bills/search.json", {headers: { "X-API-Key": key }});
-   console.log(results)
-   return results
-}
