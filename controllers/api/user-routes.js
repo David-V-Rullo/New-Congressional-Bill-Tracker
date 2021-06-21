bd06e994f46ae042c20eed1111e7e8ed17756e51
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-06-21 00:39:41 -0400

    Favorites in user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const {User} = require("../../models");
 const passport = require("../../config/passport");
+const {Favorite} = require("../../models");
 
 // Login
 router.post("/login", passport.authenticate("local"), async (req, res) => {
